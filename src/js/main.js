a5b52a5a8a25a4076d082f9bcf6c4214fa5122b3
johnwquarles <john.w.quarles@gmail.com>
2015-06-06 16:10:06 -0500

    info window works

--- src/js/main.js
+++ src/js/main.js
@@ -23,6 +23,31 @@ var mapOptions = {
 }
 var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
+var contentString = '<div id="content">'+
+    '<div id="siteNotice">'+
+    '</div>'+
+    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+    '<div id="bodyContent">'+
+    '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+    'sandstone rock formation in the southern part of the '+
+    'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+    'south west of the nearest large town, Alice Springs; 450&#160;km '+
+    '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+    'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+    'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+    'Aboriginal people of the area. It has many springs, waterholes, '+
+    'rock caves and ancient paintings. Uluru is listed as a World '+
+    'Heritage Site.</p>'+
+    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+    'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+    '(last visited June 22, 2009).</p>'+
+    '</div>'+
+    '</div>';
+
+var infowindow = new google.maps.InfoWindow({
+    content: contentString
+});
+
 // get global art installations data array
 $.get(`${apiUrl}${artResourceName}.json?${appToken}`, function(data) {
   globalArtArr = data;
@@ -32,6 +57,9 @@ $.get(`${apiUrl}${artResourceName}.json?${appToken}`, function(data) {
                   map: map,
                   title: location.name,
                   icon: artIcon
+              });
+              google.maps.event.addListener(marker, 'click', function() {
+                infowindow.open(map,marker);
               });
           });
 });
@@ -46,20 +74,25 @@ $.get(`${apiUrl}${histResourceName}.json?${appToken}`, function(data) {
                 title: location.name,
                 icon: histIcon
             });
-        });
-});
-
-// get global beer permit holders data array
-$.get(`${apiUrl}${beerResourceName}.json?${appToken}`, function(data) {
-  globalBeerArr = data;
-  console.log(globalBeerArr);
-  $.each(globalBeerArr, function(i, entry) {
-            var marker = new google.maps.Marker({
-                position: new google.maps.LatLng(entry.latitude, entry.longitude),
-                map: map,
-                title: location.name,
-                icon: beerIcon
+            google.maps.event.addListener(marker, 'click', function() {
+              infowindow.open(map,marker);
             });
         });
 });
 
+// get global beer permit holders data array
+// $.get(`${apiUrl}${beerResourceName}.json?${appToken}`, function(data) {
+//   globalBeerArr = data;
+//   console.log(globalBeerArr);
+//   $.each(globalBeerArr, function(i, entry) {
+//             var marker = new google.maps.Marker({
+//                 position: new google.maps.LatLng(entry.latitude, entry.longitude),
+//                 map: map,
+//                 title: location.name,
+//                 icon: beerIcon
+//             });
+//             google.maps.event.addListener(marker, 'click', function() {
+//               infowindow.open(map,marker);
+//             });
+//         });
+// });
