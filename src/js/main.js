f9c1589ffa3799c6f2f121b57a4cf716c7b81413
Matt Fili <matthewjosephfili@gmail.com>
2015-06-06 13:52:42 -0500

    Fixed google object

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,23 @@
-// Initialize Map Object
+// Initialize Map Object and append to map-canvas
+function initialize () {
 var mapOptions = {
   center: new google.maps.LatLng(-34.397, 150.644),
   zoom: 8
 };
-
-// Creates new map inside div map-canvas node
 var map = new google.maps.Map(document.getElementById("map-canvas"),
     mapOptions);
+}
 
 // Window Onload Listener
-google.maps.event.addDomListener(window, 'load', initialize);
+function loadScript() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' +
+      '&signed_in=true&callback=initialize';
+  document.body.appendChild(script);
+}
+
+window.onload = loadScript;
 
 
 
