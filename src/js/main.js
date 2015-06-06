4ce7d41f7fa0ff51cc8e7e83e9645c8eb1a3a6c4
johnwquarles <john.w.quarles@gmail.com>
2015-06-06 13:56:34 -0500

    global art, historical markers and beer data arrays loaded

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,26 @@
-var art_url = 'https://data.nashville.gov/resource/xakp-ess3.json?$$app_token=rTyPnE0zIrcybTe9qcUwRufiQ';
-var global_art_object;
+// these need to be put into the API queries to identify the desired datasets.
+var artResourceName = "xakp-ess3";
+var histResourceName = "vk65-u7my";
+var beerResourceName = "3wb6-xy3j";
 
-$.get(art_url, function(data) {
-  global_art_object = data
+var apiUrl = 'https://data.nashville.gov/resource/';
+var appToken = '$$app_token=rTyPnE0zIrcybTe9qcUwRufiQ';
+
+var globalArtArr;
+var globalHistArr;
+var globalBeerArr;
+
+// get global art installations data array
+$.get(`${apiUrl}${artResourceName}.json?${appToken}`, function(data) {
+  globalArtArr = data;
+});
+
+// get global historical markers data array
+$.get(`${apiUrl}${histResourceName}.json?${appToken}`, function(data) {
+  globalHistArr = data;
+});
+
+// get global beer permit holders data array
+$.get(`${apiUrl}${beerResourceName}.json?${appToken}`, function(data) {
+  globalBeerArr = data;
 });
