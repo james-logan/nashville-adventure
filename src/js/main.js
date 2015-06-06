aad1c28f51efb8ca30a74aaabd634f092c1385f6
Matt Fili <matthewjosephfili@gmail.com>
2015-06-06 14:33:40 -0500

    fixed google maps display

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,15 @@
 // Initialize Map Object and append to map-canvas
 function initialize () {
 var mapOptions = {
-  center: new google.maps.LatLng(-34.397, 150.644),
-  zoom: 8
+  center: new google.maps.LatLng(36.1565338, -86.7769905),
+  zoom: 15
 };
 var map = new google.maps.Map(document.getElementById("map-canvas"),
     mapOptions);
 }
 
-// Window Onload Listener
-function loadScript() {
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' +
-      '&signed_in=true&callback=initialize';
-  document.body.appendChild(script);
-}
+google.maps.event.addDomListener(window, 'load', initialize)
 
-window.onload = loadScript;
 
 
 
