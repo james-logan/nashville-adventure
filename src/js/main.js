534810ce8618bd1994cd9ef81504b46d4fa11a9a
James Logan <jamesblogan20@gmail.com>
2015-06-06 14:55:59 -0500

    partial functions for getting and posting from firebase

---
+++ src/js/main.js
@@ -0,0 +1,53 @@
+// these need to be put into the API queries to identify the desired datasets.
+var artResourceName = "xakp-ess3";
+var histResourceName = "vk65-u7my";
+var beerResourceName = "3wb6-xy3j";
+
+var apiUrl = 'https://data.nashville.gov/resource/';
+var appToken = '$$app_token=rTyPnE0zIrcybTe9qcUwRufiQ';
+
+var globalArtArr;
+var globalHistArr;
+var globalBeerArr;
+
+// get global art installations data array
+$.get(`${apiUrl}${artResourceName}.json?${appToken}`, function(data) {
+  globalArtArr = data;
+});
+
+// get global historical markers data array
+$.get(`${apiUrl}${histResourceName}.json?${appToken}`, function(data) {
+  globalHistArr = data;
+});
+
+// get global beer permit holders data array
+$.get(`${apiUrl}${beerResourceName}.json?${appToken}`, function(data) {
+  globalBeerArr = data;
+});
+
+
+
+// grabbing the trail as user is clicking waypoints, the data for each waypoint will be added to the trail array
+
+var trailArray = [];
+
+window.onload(function () {
+     getTrails();
+})
+
+
+function getTrails () {
+     $.get("https://nashville-adventure.firebaseio.com/trails", function (data) {
+          $(data).each(function (item) {
+               $('trail-list-div').append("<div class='trail-box'>"+item[0]+"</div>");
+          })
+     })
+}
+
+
+function postTrail () {
+     $.post('https://nashville-adventure.firebaseio.com/trails', trailArray, function (data) {
+          //trail name div needs to be replaced with noah's
+          $('trail-name-div').text(trailArray[0]);
+     });
+}
