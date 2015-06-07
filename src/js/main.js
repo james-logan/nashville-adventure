2614a7ca4de88eb3992fe196ab8a420991e99d82
Matt Fili <matthewjosephfili@gmail.com>
2015-06-06 19:23:04 -0500

    Added Routing function

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,9 @@
 var artResourceName = "xakp-ess3";
 var histResourceName = "vk65-u7my";
 var beerResourceName = "3wb6-xy3j";
+var artMarkerClick;
+var histMarkerClick;
+var waypts =[];
 
 var apiUrl = 'https://data.nashville.gov/resource/';
 var appToken = '$$app_token=rTyPnE0zIrcybTe9qcUwRufiQ';
@@ -10,65 +13,10 @@ var globalArtArr;
 var globalHistArr;
 var globalBeerArr;
 
-// get global art installations data array
-$.get(`${apiUrl}${artResourceName}.json?${appToken}`, function(data) {
-  globalArtArr = data;
-});
+var artIcon = "http://www.lavillita.com/wpimages/icon_art.png";
+var histIcon = "http://tngenweb.org/carter/graphics/tnflag.gif";
+var beerIcon = "https://cdn1.iconfinder.com/data/icons/Momentum_MatteEntireSet/32/beer.png";
 
-// get global historical markers data array
-$.get(`${apiUrl}${histResourceName}.json?${appToken}`, function(data) {
-  globalHistArr = data;
-});
-
-// get global beer permit holders data array
-$.get(`${apiUrl}${beerResourceName}.json?${appToken}`, function(data) {
-  globalBeerArr = data;
-});
-
-
-
-// grabbing the trail as user is clicking waypoints, the data for each waypoint will be added to the trail array
-
-var trailArray = [];
-
-window.onload = function () {
-     getTrails();
-}
-
-
-function getTrails () {
-     $.get("https://nashville-adventure.firebaseio.com/trails", function (data) {
-          $(data).each(function (item) {
-               $('trail-list-div').append("<div class='trail-box'>"+item[0]+"</div>");
-          })
-     })
-}
-
-
-function postTrail () {
-     $.post('https://nashville-adventure.firebaseio.com/trails', trailArray, function (data) {
-          //trail name div needs to be replaced with noah's
-          $('trail-name-div').text(trailArray[0]);
-     });
-}
-
-//************ matt's code
-
-// Initialize Map Object and append to map-canvas
-function initialize () {
-var mapOptions = {
-  center: new google.maps.LatLng(36.1565338, -86.7769905),
-  zoom: 15
-};
-var map = new google.maps.Map(document.getElementById("map-canvas"),
-    mapOptions);
-}
-
-google.maps.event.addDomListener(window, 'load', initialize)
-
-
-
-//********************************** JOHN'S CODE
 
 // set up map, put on page
 var center = new google.maps.LatLng(36.1565338,-86.7769905);
@@ -78,41 +26,121 @@ var mapOptions = {
 }
 var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-// // get global art installations data array
-// $.get(`${apiUrl}${artResourceName}.json?${appToken}`, function(data) {
-//   globalArtArr = data;
-//   $.each(globalArtArr, function(i, entry) {
-//               var marker = new google.maps.Marker({
-//                   position: new google.maps.LatLng(entry.latitude, entry.longitude),
-//                   map: map,
-//                   title: location.name
-//               });
-//           });
-// });
+var contentString = "<p>info window</p>"
 
-// // get global historical markers data array
-// $.get(`${apiUrl}${histResourceName}.json?${appToken}`, function(data) {
-//   globalHistArr = data;
-// });
+var infowindow = new google.maps.InfoWindow({
+    content: contentString
+});
 
-// // get global beer permit holders data array
+// get global art installations data array
+$.get(`${apiUrl}${artResourceName}.json?${appToken}`, function(data) {
+  globalArtArr = data;
+  $.each(globalArtArr, function(i, entry) {
+              var marker = new google.maps.Marker({
+                  position: new google.maps.LatLng(entry.latitude, entry.longitude),
+                  map: map,
+                  title: location.name,
+                  icon: artIcon
+              });
+              marker.dataWeNeed = entry;
+              google.maps.event.addListener(marker, 'click', function() {
+                infowindow.setContent(marker.dataWeNeed.description || "no description found");
+                infowindow.open(map,marker);
+
+
+              google.maps.event.addListener(marker, 'dblclick', function() {
+              var latlong = "";
+              latlong += marker.dataWeNeed.latitude;
+              latlong += ',' + marker.dataWeNeed.longitude;
+              var singleWayPt = {};
+              singleWayPt.location = latlong;
+              singleWayPt.stopover = true;
+              waypts.push(singleWayPt);
+              console.log(latlong);
+              console.log(singleWayPt);
+              if (waypts.length === 3) {
+                routeMaker(); 
+              }
+              });
+
+              });
+          });
+});
+
+// get global historical markers data array
+$.get(`${apiUrl}${histResourceName}.json?${appToken}`, function(data) {
+  globalHistArr = data;
+  $.each(globalHistArr, function(i, entry) {
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(entry.latitude, entry.longitude),
+                map: map,
+                title: location.name,
+                icon: histIcon
+            });
+            marker.dataWeNeed = entry;
+            google.maps.event.addListener(marker, 'click', function() {
+              infowindow.setContent(marker.dataWeNeed.marker_text || "no description found");
+              infowindow.open(map,marker);
+
+            google.maps.event.addListener(marker, 'dblclick', function() {
+              var latlong = "";
+              latlong += marker.dataWeNeed.latitude;
+              latlong += ',' + marker.dataWeNeed.longitude;
+              var singleWayPt = {};
+              singleWayPt.location = latlong;
+              singleWayPt.stopover = true;
+              waypts.push(singleWayPt);
+              console.log(latlong);
+              console.log(singleWayPt);
+              if (waypts.length === 3) {
+                routeMaker(); 
+              }
+              });
+
+            });
+        });
+});
+
+// get global beer permit holders data array
 // $.get(`${apiUrl}${beerResourceName}.json?${appToken}`, function(data) {
 //   globalBeerArr = data;
+//   console.log(globalBeerArr);
+//   $.each(globalBeerArr, function(i, entry) {
+//             var marker = new google.maps.Marker({
+//                 position: new google.maps.LatLng(entry.latitude, entry.longitude),
+//                 map: map,
+//                 title: location.name,
+//                 icon: beerIcon
+//             });
+//             google.maps.event.addListener(marker, 'click', function() {
+//               infowindow.open(map,marker);
+//             });
+//         });
 // });
 
-plotPoints(artResourceName);
-plotPoints(histResourceName);
-plotPoints(beerResourceName, "http://www.nowplayingnashville.com/sites/nowplayingnashville.com/images/org/220173834/yazoo_small.png");
-function plotPoints(resourceName, iconName) {
-  $.get(`${apiUrl}${resourceName}.json?${appToken}`, function(data) {
-    $.each(data, function(i, entry) {
-                var marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(entry.latitude, entry.longitude),
-                    map: map,
-                    title: location.name,
-                    icon: iconName
-                });
-            });
-  });
-}
 
+// Dynamic Flux Capacitor Routing
+var directionsDisplay 
+var directionsService = new google.maps.DirectionsService();
+var map;
+directionsDisplay = new google.maps.DirectionsRenderer();
+directionsDisplay.setMap(map)
+
+
+var routeMaker = function() {
+
+  var request = {
+      origin: waypts[0].location,
+      destination: waypts[waypts.length-1].location,
+      waypoints: waypts,
+      optimizeWaypoints: true,
+      travelMode: google.maps.TravelMode.WALKING
+  };
+
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+      var route = response.routes[0];
+      };
+    });
+}
